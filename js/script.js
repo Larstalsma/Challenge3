09837e379389e2fd24751421110d8bdcf8ca20e2
Larstalsma <47833022+Larstalsma@users.noreply.github.com>
2019-04-05 19:55:44 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 /*------------------------------------------------------------------------------ WEATHER API -------------------------------------------------------------------------------*/
 
-function getWeatherData(city, id, id2) {
+function getWeatherData(city, id, id2) { //retrieve weather data from OpenWeather API
 
     var url = "https://api.openweathermap.org/data/2.5/weather";
     var apiKey ="b0c8dafa512a0134e90df6ece3c2b7a2";
 
-    var request = url + "?" + "appid=" + apiKey + "&" + "q=" + city;
+    var request = url + "?" + "appid=" + apiKey + "&" + "q=" + city; //creating the retrieval URL within a var
     
-    fetch(request)
+    fetch(request) //fetching the data through the link
     
     .then(function(response) {
         if(!response.ok) throw Error(response.statusText);
@@ -23,8 +23,8 @@ function getWeatherData(city, id, id2) {
     });
 }
 
-function tempIcon(degC) {
-  if (degC < 0)
+function tempIcon(degC) { //retrieving temperature icons from FontAwesome
+  if (degC < 0) //if-statement determining which icon is to be written into the html
     return '<i class="fas fa-thermometer-empty"></i> ';
   if (degC < 10)
     return '<i class="fas fa-thermometer-quarter"></i> ';
@@ -36,20 +36,22 @@ function tempIcon(degC) {
 return '<i class="fas fa-thermometer-full"></i> ';
 }
 
-function onWeatherSucces(response, id, id2) {
-
-    var type = '<img class="weatherIcon" src="images/'+response.weather[0].icon+'.png" />';
+function onWeatherSucces(response, id, id2) { //the continuation of the getWeatherData function.  
+//The parameters are: response, which takes the data from the getWeatherData function; id, which is the id where the weather information 
+//will be written to; id2, which is the div that the Elevation data will be written to. 
+    
+    var type = '<img class="weatherIcon" src="images/'+response.weather[0].icon+'.png" />'; //determining which locally saved img to use
     var windSpeed = response.wind.speed;
     var degC = Math.floor(response.main.temp - 273.15);
     var temperatureIcon = tempIcon(degC);
     var windIcon = '<i class="fas fa-wind"></i>';
 
-    var weatherBox = document.getElementById(id);
+    var weatherBox = document.getElementById(id); 
     weatherBox.innerHTML += type + "<br />" + '<span class="degC">' + temperatureIcon + degC + "&#176;C <br />" + '</span>' + '<span class="windSpeed">' + windIcon + windSpeed + " m/s" + '</span>';
     getElevationData(response.coord.lat,response.coord.lon, id2);
 }
 
-function onWeatherError(error, id) {
+function onWeatherError(error, id) { //error function in case the API data can not be retrieved
     console.error('Request failed', error);
     var weatherBox = document.getElementById(id);
     weatherBox.className = 'hidden'; 
@@ -59,10 +61,11 @@ function onWeatherError(error, id) {
 /*------------------------------------------------------------------------------ ELEVATION API ------------------------------------------------------------------------------*/
 
 
-function getElevationData(latitude, longitude, id2) {
+function getElevationData(latitude, longitude, id2) { //the elevation data based off of the cities used in the weather function
     var request = 'https://open.mapquestapi.com/elevation/v1/profile?key=uV4sKUUwnFl2Cqps8bNb3SKvVXyNoQLk&shapeFormat=raw&latLngCollection=' + latitude + "," + longitude;
-    var mountainIcon = '<i class="fas fa-mountain"></i>';
-    fetch(request)
+    //requesting the API for data through this url
+    var mountainIcon = '<i class="fas fa-mountain"></i>'; //using a mountain icon from FontAwesome to display along with elevation
+    fetch(request) //fetching the data from the API
 
     .then(function(response) {
         if(!response.ok) throw Error(response.statusText);
@@ -72,6 +75,7 @@ function getElevationData(latitude, longitude, id2) {
     .then(function(response) {
 
         document.getElementById(id2).innerHTML += '<span class="elevationText">' + mountainIcon + response.elevationProfile[0].height + 'm' + '</span>';
+    //writing everything into the declared id2
     }).catch(function(error) {
         console.error(error);
     });
@@ -81,7 +85,7 @@ function getElevationData(latitude, longitude, id2) {
 /*--------------------------------------------------------------------------- HEADER CLOCK & DATE ----------------------------------------------------------------------------*/
 
 
-function showTime(){
+function showTime(){ //general function to show the time in the upper left corner
     var date = new Date(Date.now());
     var h = date.getHours(); // 0 - 23
     var m = date.getMinutes(); // 0 - 59
@@ -96,11 +100,11 @@ function showTime(){
     setTimeout(showTime, 1000);
 }
 
-function showDate(){
+function showDate(){ //general function to show the date in the upper right corner
     var date = new Date(Date.now());
     var day = date.getDate(); // 1 - 31
     var month = date.getMonth() +1; // 1 - 12
-    var year = date.getFullYear() +49;
+    var year = date.getFullYear() +49; //displaying the year 2068, in which the ITS will come back to earth
 
     day = (day < 10) ? "0" + day : day;
     month = (month < 10) ? "0" + month : month;
@@ -111,7 +115,7 @@ function showDate(){
 
 
 /*-------------------------------------------------------------------------- LOADING API FUNCTIONS --------------------------------------------------------------------------*/
-
+//calling all functions
 getWeatherData('Cape Canaveral, us','art1','art4');
 getWeatherData('Baikonur, kz','art2','art5');
 getWeatherData('White Sands, us','art3','art6');
